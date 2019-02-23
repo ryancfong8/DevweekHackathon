f59da1ccd4e12bc6ec76b8be4fa92f26c63a7031
Vinson Chen <viz@Vinsons-MacBook-Pro.local>
2019-02-23 15:42:37 -0800

    build main page

---
+++ frontend/components/default.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Default extends Component {
+  render() {
+    return (
+      <div className="contruction-container">
+        <h1>Hello world!</h1>
+      </div>
+    );
+  }
+}
+
+export default Default;
