af1350783cf938f055b14a30a432db005c4b6ae3
Tony Xie <tony_xie@outlook.com>
2019-02-23 15:40:41 -0800

    trip controller and routes

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,2 +1,36 @@
 class TripsController < ApplicationController
+  def index
+    render json: Trip.all
+  end
+  
+  def new
+    render json: Trip.new
+  end
+  
+  def show
+    @trip = Trip.find(params[:id])
+    render json: @trip
+  end
+  
+  def update
+    @trip = Trip.find(params[:id])
+    @trip.update(params)
+    
+    render json: @trip
+  end
+  
+  def destroy
+    @trip = Trip.find(params[:id])
+    @trip.destroy
+    return
+  end
+  
+  private
+  
+  def trip_params
+    params.require(:article).permit(:id, :name, :start_date, :start_time, :end_date, :end_time, 
+      :itinerary, :transportation, :drop_off_location_long, :drop_off_location_lat, 
+      :pick_up_location_long, :pick_up_location_lat
+    )
+  end
 end
